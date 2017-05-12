af350eb2004d7cac5c09772b0b99e72305ac62ce
inwin <jv.thomasmore@gmail.com>
2017-05-12 14:27:23 +0200

    V1.0
    string ontvangen
    bug bij kleur weergave

---
+++ wdaUartTest/wdaUartTest/Debug/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc.d
@@ -0,0 +1,89 @@
+Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc.o: \
+ ../Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_rcc.c \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Inc/stm32f7xx_hal_conf.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Inc/main.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f746xx.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Include/core_cm7.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_spi.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h \
+ C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Inc/stm32f7xx_hal_conf.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Inc/main.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_def.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f7xx.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Device/ST/STM32F7xx/Include/stm32f746xx.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Include/core_cm7.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/CMSIS/Device/ST/STM32F7xx/Include/system_stm32f7xx.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_rcc_ex.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_gpio_ex.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_dma_ex.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_cortex.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_flash_ex.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_i2c_ex.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_pwr_ex.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_spi.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart.h:
+
+C:/Users/r0631/Documents/documenten\ elektronica-ict/fase2\ sem2/labview/project/wdaUartTest/wdaUartTest/Drivers/STM32F7xx_HAL_Driver/Inc/stm32f7xx_hal_uart_ex.h:
